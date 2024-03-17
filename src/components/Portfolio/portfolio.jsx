eaa9ebcdf9d937e37ca420be1fc9f2ef9356ed76
maryan-Hassan <Manishakaay@gmail.com>
2024-03-17 05:04:20 -0700

    first commit

---
+++ src/components/Portfolio/portfolio.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import css from "./Portfolio.module.scss";
+import { motion } from "framer-motion";
+import {
+  fadeIn,
+  footerVariants,
+  staggerChildren,
+  textVariant,
+  textVariant2,
+} from "../../utils/motion";
+import { myProjects } from "../../utils/data";
+const Slick = () => {
+  function Arrow(props) {
+    const { className, style, onClick } = props;
+    return (
+      <div
+        className={className}
+        style={{
+          ...style,
+          display: "block",
+          // background: "wheat",
+          // borderRadius: "10px",
+        }}
+        onClick={onClick}
+      />
+    );
+  }
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 400,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    nextArrow: <Arrow />,
+    prevArrow: <Arrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <motion.section
+      variants={staggerChildren}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className="paddings"
+    >
+      <a className="anchor" id="portfolio"></a>
+
+      <motion.div variants={textVariant(0.4)} className={`  ${css.heading}`}>
+        <div>
+          <span className="primaryText">My Latest Works</span>
+          <p style={{ marginTop: "10px" }}>
+            Perfect solution for digital experience
+          </p>
+        </div>
+      </motion.div>
+      <motion.div variants={footerVariants}>
+        <Slider {...settings} className={css.slider}>
+          {myProjects.map((project, i) => {
+            return (
+              <a href={project.link}>
+                <div className={css.project}>
+                  <h3>{project.projectName}</h3>
+                  <img src={project.image} alt="App-image" />
+                </div>
+              </a>
+            );
+          })}
+        </Slider>
+      </motion.div>
+    </motion.section>
+  );
+};
+
+export default Slick;
---
+++ src/components/hero/Hero.jsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { fadeIn, slideIn, staggerContainer } from "../../utils/motion";
+import css from "./Hero.module.scss";
+import { motion } from "framer-motion";
+const Hero = () => {
+  return (
+    <section className={`paddings ${css.wrapper}`}>
+      <motion.div
+        variants={staggerContainer}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: false, amount: 0.25 }}
+        className={`innerWidth ${css.container}`}
+      >
+        <div className={css.upperElements}>
+          <motion.span
+            className="primaryText"
+            variants={fadeIn("right", "tween", 0.2, 1)}
+          >
+            Hey There,
+            <br />
+            I'm Maryam
+            <br />
+            Hassan.
+          </motion.span>
+          <motion.span
+            className="secondaryText"
+            variants={fadeIn("left", "tween", 0.4, 1)}
+          >
+            I design beautiful and magical
+            <br />
+            things, And I love what i do.
+          </motion.span>
+        </div>
+
+        <motion.div
+          variants={fadeIn("up", "tween", 0.3, 1)}
+          className={css.person}
+        >
+          <motion.img
+            variants={slideIn("up", "tween", 0.5, 1.3)}
+            src="./maria.png"
+            alt=""
+          />
+        </motion.div>
+
+        <a className={css.email} href="mailto: abdirizackissack2018@gmail.com">
+          maryamhassan"gmai.com"
+        </a>
+
+        <div className={css.lowerElements}>
+          <motion.div
+            variants={fadeIn("right", "tween", 0.3, 1)}
+            className={css.experience}
+          >
+            <div className="primaryText">3</div>
+            <div className="secondaryText">
+              <div>Years</div>
+              <div>Experience</div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            variants={fadeIn("left", "tween", 0.5, 1)}
+            className={css.certificate}
+          >
+            <img src="./certificate.png" alt="" />
+            <span>CERTIFIED PROFESSIONAL</span>
+            <span>FORNTEND DEVELOPER</span>
+          </motion.div>
+        </div>
+      </motion.div>
+    </section>
+  );
+};
+
+export default Hero;
